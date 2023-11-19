a7f7784548b352797dc7ba019cf9a798af164d26
Brendan <bmmeehan3@gmail.com>
2023-11-19 15:52:07 -0500

    added runlogging

--- database.go
+++ database.go
@@ -1,129 +1,179 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    _ "github.com/microsoft/go-mssqldb"
-    "log"
-    "net/url"
-    "os"
-    "strconv"
+	"database/sql"
+	"fmt"
+	_ "github.com/microsoft/go-mssqldb"
+	"log"
+	"net/url"
+	"strconv"
 )
 
+type Result[T any] struct {
+	Value T
+}
 type IpAddress struct {
-    IpAddressId int    `json:"ipaddressid"`
-    IpAddress   string `json:"ipaddress"`
-    IsActive    bool   `json:"isactive"`
-    DateCreated string `json:"datecreated"`
+	IpAddressId int    `json:"ipaddressid"`
+	IpAddress   string `json:"ipaddress"`
+	IsActive    bool   `json:"isactive"`
+	DateCreated string `json:"datecreated"`
+}
+type IpRunLog struct {
+	IpRunLogId int    `json:"iprunlogid"`
+	RunDate    string `json:"rundate"`
+	IpUpdated  bool   `json:"ipupdated"`
 }
 
 func setupDb() *sql.DB {
-    var db *sql.DB
-    dbUser := os.Getenv("DB_USER")
-    dbPass := os.Getenv("DB_PASS")
-    dbIp := os.Getenv("DB_IP")
-    dbPort := os.Getenv("DB_PORT")
-    dbPortAsInt, convertErr := strconv.Atoi(dbPort)
-    if convertErr != nil {
-        fmt.Errorf("error setting up db, cant parse env var DB_PORT")
-        return nil
-    }
-    query := url.Values{}
-    query.Add("database", "General")
+	var db *sql.DB
+	//dbUser := os.Getenv("DB_USER")
+	dbUser := "bmtron"
+	//dbPass := os.Getenv("DB_PASS")
+	dbPass := "Burtreynolds#30"
+	//dbIp := os.Getenv("DB_IP")
+	dbIp := "192.168.50.16"
+	//dbPort := os.Getenv("DB_PORT")
+	dbPort := "1433"
+	dbPortAsInt, convertErr := strconv.Atoi(dbPort)
+	if convertErr != nil {
+		fmt.Errorf("error setting up db, cant parse env var DB_PORT")
+		return nil
+	}
+	query := url.Values{}
+	query.Add("database", "General")
 
-    u := &url.URL{
-        Scheme:   "sqlserver",
-        User:     url.UserPassword(dbUser, dbPass),
-        Host:     fmt.Sprintf("%s:%d", dbIp, dbPortAsInt),
-        RawQuery: query.Encode(),
-    }
+	u := &url.URL{
+		Scheme:   "sqlserver",
+		User:     url.UserPassword(dbUser, dbPass),
+		Host:     fmt.Sprintf("%s:%d", dbIp, dbPortAsInt),
+		RawQuery: query.Encode(),
+	}
 
-    var err error
-    db, err = sql.Open("sqlserver", u.String())
-    if err != nil {
-        log.Fatal(err)
-    }
+	var err error
+	db, err = sql.Open("sqlserver", u.String())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    pingErr := db.Ping()
-    if pingErr != nil {
-        log.Fatal(err)
-    }
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(err)
+	}
 
-    return db
+	return db
 }
 
 func getAllIpAddresses(db *sql.DB) ([]IpAddress, error) {
-    var ips []IpAddress
+	var ips []IpAddress
 
-    rows, err := db.Query(`select IpAddressId, IpAddress, IsActive, DateCreated from IpAddress;`)
-    if err != nil {
-        return nil, fmt.Errorf("ipaddresses %v", err)
-    }
-    defer rows.Close()
+	rows, err := db.Query(`select IpAddressId, IpAddress, IsActive, DateCreated from IpAddress;`)
+	if err != nil {
+		return nil, fmt.Errorf("ipaddresses %v", err)
+	}
+	defer rows.Close()
 
-    for rows.Next() {
-        var ip IpAddress
-        if err := rows.Scan(&ip.IpAddressId, &ip.IpAddress, &ip.IsActive, &ip.DateCreated); err != nil {
-            return nil, fmt.Errorf("ipaddresses %v", err)
-        }
-        ips = append(ips, ip)
-    }
-    if err := rows.Err(); err != nil {
-        return nil, fmt.Errorf("ipaddresses %v", err)
-    }
+	for rows.Next() {
+		var ip IpAddress
+		if err := rows.Scan(&ip.IpAddressId, &ip.IpAddress, &ip.IsActive, &ip.DateCreated); err != nil {
+			return nil, fmt.Errorf("ipaddresses %v", err)
+		}
+		ips = append(ips, ip)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("ipaddresses %v", err)
+	}
 
-    return ips, nil
+	return ips, nil
 }
 
-func getActiveIpAddresses(db *sql.DB) ([]IpAddress, error) {
-    var ips []IpAddress
+func getActiveIpAddresses(db *sql.DB) (Result[[]IpAddress], error) {
+	var ips []IpAddress
 
-    rows, err := db.Query(`select IpAddressId, IpAddress, IsActive, DateCreated from IpAddress where IsActive = 1;`)
-    if err != nil {
-        return nil, fmt.Errorf("ipadresses: active %v", err)
-    }
-    defer rows.Close()
+	rows, err := db.Query(`select IpAddressId, IpAddress, IsActive, DateCreated from IpAddress where IsActive = 1;`)
+	if err != nil {
+		return Result[[]IpAddress]{}, fmt.Errorf("ipadresses: active %v", err)
+	}
+	defer rows.Close()
 
-    for rows.Next() {
-        var ip IpAddress
-        if err := rows.Scan(&ip.IpAddressId, &ip.IpAddress, &ip.IsActive, &ip.DateCreated); err != nil {
-            return nil, fmt.Errorf("ipaddresses: active %v", err)
-        }
-        ips = append(ips, ip)
-    }
-    if err := rows.Err(); err != nil {
-        return nil, fmt.Errorf("ipaddresses %v", err)
-    }
-
-    return ips, nil
+	for rows.Next() {
+		var ip IpAddress
+		if err := rows.Scan(&ip.IpAddressId, &ip.IpAddress, &ip.IsActive, &ip.DateCreated); err != nil {
+			return Result[[]IpAddress]{}, fmt.Errorf("ipaddresses: active %v", err)
+		}
+		ips = append(ips, ip)
+	}
+	if err := rows.Err(); err != nil {
+		return Result[[]IpAddress]{}, fmt.Errorf("ipaddresses %v", err)
+	}
+	var res = Result[[]IpAddress]{}
+	res.Value = ips
+	return res, nil
 }
 
 func deactivateIpAddresses(db *sql.DB) (int64, error) {
 
-    res, err := db.Exec(`UPDATE IpAddress SET IsActive = 0;`)
+	res, err := db.Exec(`UPDATE IpAddress SET IsActive = 0;`)
 
-    if err != nil {
-        return 0, fmt.Errorf("ipaddress: deactivate %v", err)
-    }
+	if err != nil {
+		return 0, fmt.Errorf("ipaddress: deactivate %v", err)
+	}
 
-    rowsAffected, rowErr := res.RowsAffected()
-    if rowErr != nil {
-        return 0, fmt.Errorf("ipaddress: deactivate %v", rowErr)
-    }
-    return rowsAffected, nil
+	rowsAffected, rowErr := res.RowsAffected()
+	if rowErr != nil {
+		return 0, fmt.Errorf("ipaddress: deactivate %v", rowErr)
+	}
+	return rowsAffected, nil
 }
 
 func insertNewIpAddress(db *sql.DB, address IpAddress) (int64, error) {
-    res, err := db.Exec(`INSERT INTO IpAddress (IpAddress, IsActive, DateCreated) VALUES (@ipaddr, @active, getdate());`,
-        sql.Named("ipaddr", &address.IpAddress), sql.Named("active", &address.IsActive))
-    if err != nil {
-        return 0, fmt.Errorf("ippaddress: add %v", err)
-    }
+	res, err := db.Exec(`INSERT INTO IpAddress (IpAddress, IsActive, DateCreated) VALUES (@ipaddr, @active, getdate());`,
+		sql.Named("ipaddr", &address.IpAddress), sql.Named("active", &address.IsActive))
+	if err != nil {
+		return 0, fmt.Errorf("ippaddress: add %v", err)
+	}
 
-    rowsAffected, rowErr := res.RowsAffected()
-    if rowErr != nil {
-        return 0, fmt.Errorf("ipaddress: deactivate %v", rowErr)
-    }
+	rowsAffected, rowErr := res.RowsAffected()
+	if rowErr != nil {
+		return 0, fmt.Errorf("ipaddress: deactivate %v", rowErr)
+	}
 
-    return rowsAffected, nil
+	return rowsAffected, nil
+}
+func getAllRunLogs(db *sql.DB) (Result[[]IpRunLog], error) {
+	var runLogs []IpRunLog
+	var res Result[[]IpRunLog]
+	rows, err := db.Query(`SELECT IpRunLogId, RunDate, IpUpdated FROM IpRunLog;`)
+	if err != nil {
+		return Result[[]IpRunLog]{}, fmt.Errorf("iprunlog: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var runLog IpRunLog
+		if err := rows.Scan(&runLog.IpRunLogId, &runLog.RunDate, &runLog.IpUpdated); err != nil {
+			return Result[[]IpRunLog]{}, fmt.Errorf("iprunlog: %v", err)
+		}
+
+		runLogs = append(runLogs, runLog)
+	}
+
+	if err := rows.Err(); err != nil {
+		return Result[[]IpRunLog]{}, fmt.Errorf("iprunlog: %v", err)
+	}
+	res.Value = runLogs
+	return res, nil
+}
+
+func addNewRunLog(db *sql.DB, runLog IpRunLog) (int64, error) {
+	res, err := db.Exec(`INSERT INTO IpRunLog (RunDate, IpUpdated) VALUES (@rundate, @updated);`,
+		sql.Named("rundate", runLog.RunDate), sql.Named("updated", runLog.IpUpdated))
+	if err != nil {
+		return 0, fmt.Errorf("add runlog: %v", err)
+	}
+	rowsAffected, rowErr := res.RowsAffected()
+	if rowErr != nil {
+		return 0, fmt.Errorf("add runlog: %v", err)
+	}
+
+	return rowsAffected, nil
 }
--- main.go
+++ main.go
@@ -1,55 +1,76 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"database/sql"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var db *sql.DB
 
 func main() {
-    router := gin.Default()
-    db = setupDb()
-    router.GET("/ip", getIps)
-    router.GET("/ip/active", getActiveIps)
-    router.PUT("/ip/deactivateall", deactivateIps)
-    router.POST("/ip/new", addNewIp)
+	router := gin.Default()
+	db = setupDb()
+	router.GET("/ip", getIps)
+	router.GET("/ip/active", getActiveIps)
+	router.PUT("/ip/deactivateall", deactivateIps)
+	router.POST("/ip/new", addNewIp)
 
-    router.Run("localhost:8080")
+	router.GET("/ip/runlog", getRunLogs)
+	router.POST("/ip/runlog/new", insertRunLog)
+
+	router.Run("localhost:8080")
 }
 
 func getIps(c *gin.Context) {
-    ipAdresses, _ := getAllIpAddresses(db)
-    c.IndentedJSON(http.StatusOK, ipAdresses)
+	ipAdresses, _ := getAllIpAddresses(db)
+	c.IndentedJSON(http.StatusOK, ipAdresses)
 }
 
 func getActiveIps(c *gin.Context) {
-    ipAddresses, _ := getActiveIpAddresses(db)
-    c.IndentedJSON(http.StatusOK, ipAddresses)
+	ipAddresses, _ := getActiveIpAddresses(db)
+	c.IndentedJSON(http.StatusOK, ipAddresses)
 }
 
 func deactivateIps(c *gin.Context) {
-    rowsAffected, err := deactivateIpAddresses(db)
-    fmt.Sprintf("%d total rows affected.", rowsAffected)
-    if err != nil {
-        return
-    }
-    c.IndentedJSON(http.StatusNoContent, gin.H{"result": "entities modified successfully"})
+	rowsAffected, err := deactivateIpAddresses(db)
+	fmt.Sprintf("%d total rows affected.", rowsAffected)
+	if err != nil {
+		return
+	}
+	c.IndentedJSON(http.StatusNoContent, gin.H{"result": "entities modified successfully"})
 }
 
 func addNewIp(c *gin.Context) {
-    var newIp IpAddress
+	var newIp IpAddress
 
-    if err := c.BindJSON(&newIp); err != nil {
-        return
-    }
-    rowsAffected, err := insertNewIpAddress(db, newIp)
-    fmt.Sprintf("%d total rows affected.", rowsAffected)
-    if err != nil {
-        return
-    }
-    c.IndentedJSON(http.StatusCreated, gin.H{"result": "entity added successfully"})
+	if err := c.BindJSON(&newIp); err != nil {
+		return
+	}
+	rowsAffected, err := insertNewIpAddress(db, newIp)
+	fmt.Sprintf("%d total rows affected.", rowsAffected)
+	if err != nil {
+		return
+	}
+	c.IndentedJSON(http.StatusCreated, gin.H{"result": "entity added successfully"})
+}
 
+func getRunLogs(c *gin.Context) {
+	ipRunLogs, _ := getAllRunLogs(db)
+	c.IndentedJSON(http.StatusOK, ipRunLogs)
+}
+
+func insertRunLog(c *gin.Context) {
+	var newRunLog IpRunLog
+
+	if err := c.BindJSON(&newRunLog); err != nil {
+		return
+	}
+	rowsAffected, err := addNewRunLog(db, newRunLog)
+	fmt.Sprintf("%d total rows affected", rowsAffected)
+	if err != nil {
+		return
+	}
+	c.IndentedJSON(http.StatusCreated, gin.H{"result": "entity added successfully"})
 }
